f05710dcdcab16493f59284593ecb5cb0cb42bce
ElsaKrasniqii <elsa.krasniqi12@student.uni-pr.edu>
2025-12-19 08:25:45 +0100

    update i student klases qe te pershtatet me kerkesa te studentService

--- src/Student.java
+++ src/Student.java
@@ -1,14 +1,17 @@
 public class Student {
+
     private int id;
     private String name;
     private double gpa;
 
+    // Constructor
     public Student(int id, String name, double gpa) {
         this.id = id;
         this.name = name;
         this.gpa = gpa;
     }
 
+    // Getters
     public int getId() {
         return id;
     }
@@ -21,12 +24,27 @@ public class Student {
         return gpa;
     }
 
+    // Setters
     public void setName(String name) {
         this.name = name;
     }
 
     public void setGpa(double gpa) {
         this.gpa = gpa;
+    }
+
+    // Convert student to file format
+    public String toFileString() {
+        return id + "," + name + "," + gpa;
+    }
+
+    // Create student object from file line
+    public static Student fromFileString(String line) {
+        String[] parts = line.split(",");
+        int id = Integer.parseInt(parts[0]);
+        String name = parts[1];
+        double gpa = Double.parseDouble(parts[2]);
+        return new Student(id, name, gpa);
     }
 
     @Override
